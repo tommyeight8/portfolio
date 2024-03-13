ebd9203338ba1f23933c3b778ce6b04ff79e03f5
tommy <tommyvong88@gmail.com>
2024-03-12 23:06:06 -0700

    update project component

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -3,8 +3,13 @@ import Image from 'next/image'
 import React from 'react'
 import project from '@/schemas/project'
 import Link from 'next/link'
+import { Project } from '@/lib/types'
 
-const Project = ({ project }) => {
+type Props = {
+  project: Project
+}
+
+const Project = ({ project }: Props) => {
   return (
     <div
     className='relative w-56 h-56 rounded-md object-cover 
