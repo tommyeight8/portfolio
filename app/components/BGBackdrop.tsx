22e2a997dffbe3aefeb368cf3d8bce6c1722316a
tommy <tommyvong88@gmail.com>
2024-03-13 20:45:04 -0700

    add back bgbackdrop

--- app/components/BGBackdrop.tsx
+++ app/components/BGBackdrop.tsx
@@ -6,27 +6,27 @@ const BGBackdrop = () => {
     <div className='absolute top-0 left-0 flex itemcn justify-center h-screen w-full 
         -z-10 overflow-hidden bg-[#07101f]'>
        <div className={`${styles.bubbles} relative flex w-full max-w-[900px]`}>
-            <span style={{['--i' as any]: 11}}></span>
-            <span style={{['--i' as any]: 12}}></span>
-            <span style={{['--i' as any]: 24}}></span>
-            <span style={{['--i' as any]: 10}}></span>
-            <span style={{['--i' as any]: 14}}></span>
-            <span style={{['--i' as any]: 23}}></span>
-            <span style={{['--i' as any]: 18}}></span>
-            <span style={{['--i' as any]: 16}}></span>
-            <span style={{['--i' as any]: 19}}></span>
-            <span style={{['--i' as any]: 20}}></span>
-            <span style={{['--i' as any]: 25}}></span>
-            <span style={{['--i' as any]: 18}}></span>
-            <span style={{['--i' as any]: 21}}></span>
-            <span style={{['--i' as any]: 13}}></span>
-            <span style={{['--i' as any]: 15}}></span>
-            <span style={{['--i' as any]: 26}}></span>
-            <span style={{['--i' as any]: 17}}></span>
-            <span style={{['--i' as any]: 13}}></span>
-            <span style={{['--i' as any]: 28}}></span>
-            <span style={{['--i' as any]: 18}}></span>
-            <span style={{['--i' as any]: 16}}></span>
+            <span style={{['--i' as string]: 11}}></span>
+            <span style={{['--i' as string]: 12}}></span>
+            <span style={{['--i' as string]: 24}}></span>
+            <span style={{['--i' as string]: 10}}></span>
+            <span style={{['--i' as string]: 14}}></span>
+            <span style={{['--i' as string]: 23}}></span>
+            <span style={{['--i' as string]: 18}}></span>
+            <span style={{['--i' as string]: 16}}></span>
+            <span style={{['--i' as string]: 19}}></span>
+            <span style={{['--i' as string]: 20}}></span>
+            <span style={{['--i' as string]: 25}}></span>
+            <span style={{['--i' as string]: 18}}></span>
+            <span style={{['--i' as string]: 21}}></span>
+            <span style={{['--i' as string]: 13}}></span>
+            <span style={{['--i' as string]: 15}}></span>
+            <span style={{['--i' as string]: 26}}></span>
+            <span style={{['--i' as string]: 17}}></span>
+            <span style={{['--i' as string]: 13}}></span>
+            <span style={{['--i' as string]: 28}}></span>
+            <span style={{['--i' as string]: 18}}></span>
+            <span style={{['--i' as string]: 16}}></span>
        </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className='scroll-smooth' suppressHydrationWarning>
       <body className={`${inter.className} bg-[#07101f]`}>
           <ActiveContextProvider>
               <Header2 />
